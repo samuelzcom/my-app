a2223f61814cf0ce63ab93bc3b23bb193e0c74ec
Samuelz <al@samuelz.com>
2025-08-21 10:20:49 +0200

    Manage OPENAI_API_KEY

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,10 +1,13 @@
+import dotenv from "dotenv";
 import express, { type Request, type Response } from "express";
+
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+  res.send("Hello World");
 });
 
 app.listen(port, () => {
