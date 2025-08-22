69e8262ce9a5c77dcbe86d9e51125795c36a2aa8
Samuelz <al@samuelz.com>
2025-08-22 14:30:32 +0200

    Building the Chat API

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,9 +1,15 @@
 import dotenv from 'dotenv';
 import express, { type Request, type Response } from 'express';
+import OpenAI from 'openai';
 
 dotenv.config();
 
+const client = new OpenAI({
+   apiKey: process.env.OPENAI_API_KEY,
+});
+
 const app = express();
+app.use(express.json());
 const port = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
@@ -12,6 +18,19 @@ app.get('/', (req: Request, res: Response) => {
 
 app.get('/api/hello', (req: Request, res: Response) => {
    res.json({ message: 'Hello World!' });
+});
+
+app.post('/api/chat', async (req: Request, res: Response) => {
+   const { prompt } = req.body;
+
+   const response = await client.responses.create({
+      model: 'gpt-5-nano',
+      input: prompt,
+      temperature: 0.5,
+      max_output_tokens: 100,
+   });
+
+   res.json({ message: response.output_text });
 });
 
 app.listen(port, () => {
