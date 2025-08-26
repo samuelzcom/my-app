606120715e7d4c99cb9e95ef1dddc12b64b1dfaf
Samuelz <al@samuelz.com>
2025-08-26 10:52:34 +0200

    Refactor: Extracting Routes

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,22 +1,14 @@
 import dotenv from 'dotenv';
-import express, { type Request, type Response } from 'express';
-import { chatController } from './controllers/chat.controller';
+import express from 'express';
+import router from './routes';
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(router);
+
 const port = process.env.PORT || 3000;
-
-app.get('/', (req: Request, res: Response) => {
-   res.send('Hello World!');
-});
-
-app.get('/api/hello', (req: Request, res: Response) => {
-   res.json({ message: 'Hello World!' });
-});
-
-app.post('/api/chat', chatController.sendMessage);
 
 app.listen(port, () => {
    console.log(`Server is running on http://localhost:${port}`);
---
+++ packages/server/routes.ts
@@ -0,0 +1,16 @@
+import express, { type Request, type Response } from 'express';
+import { chatController } from './controllers/chat.controller';
+
+const router = express.Router();
+
+router.get('/', (req: Request, res: Response) => {
+   res.send('Hello World!');
+});
+
+router.get('/api/hello', (req: Request, res: Response) => {
+   res.json({ message: 'Hello World!' });
+});
+
+router.post('/api/chat', chatController.sendMessage);
+
+export default router;
