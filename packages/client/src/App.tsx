2cc4e23aeb44d2f92360c91b9cc6151e465ca44b
Samuelz <al@samuelz.com>
2025-08-27 06:47:36 +0200

    Building The ChatBot Component

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,20 +1,10 @@
-import { useEffect, useState } from 'react';
 import './App.css';
-import { Button } from './components/ui/button';
+import Chatbot from './components/Chatbot';
 
 function App() {
-   const [message, setMessage] = useState('');
-
-   useEffect(() => {
-      fetch('/api/hello')
-         .then((res) => res.json())
-         .then((data) => setMessage(data.message));
-   }, []);
-
    return (
       <div className="p-4">
-         <p className="font-bold text-3xl">{message}</p>
-         <Button>Click me!</Button>
+         <Chatbot />
       </div>
    );
 }
