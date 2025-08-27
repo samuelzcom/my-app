6b70bd500ea2fdc345aa140e09fb4b1e93a0a8a4
Samuelz <al@samuelz.com>
2025-08-27 09:24:20 +0200

    Improving Copy Behaviour

--- packages/client/src/components/Chatbot.tsx
+++ packages/client/src/components/Chatbot.tsx
@@ -49,12 +49,20 @@ const Chatbot = () => {
       }
    };
 
+   const onCopyMessage = (e: React.ClipboardEvent<HTMLDivElement>) => {
+      const selection = window.getSelection()?.toString().trim();
+      if (selection) {
+         e.preventDefault();
+         e.clipboardData.setData('text/plain', selection);
+      }
+   };
    return (
       <div>
          <div className="flex flex-col gap-3 mb-10">
             {messages.map((message, index) => (
                <div
                   key={index}
+                  onCopy={onCopyMessage}
                   className={`px-3 py-1 rounded-xl ${
                      message.role === 'user'
                         ? 'bg-blue-600 text-white self-end'
