12893bff608fb2e342803f852db3892a36437aa4
Samuelz <al@samuelz.com>
2025-08-28 09:28:40 +0200

    Improving Chatbot Responses

--- packages/server/services/chat.service.ts
+++ packages/server/services/chat.service.ts
@@ -1,10 +1,19 @@
+import fs from 'fs';
 import OpenAI from 'openai';
+import path from 'path';
+import template from '../prompts/chatbot.txt';
 import { conversationRepository } from '../repositories/conversation.repository';
 
 // Implementation detail
 const client = new OpenAI({
    apiKey: process.env.OPENAI_API_KEY,
 });
+
+const parkInfo = fs.readFileSync(
+   path.join(__dirname, '..', 'prompts', 'WonderWorld.md'),
+   'utf-8'
+);
+const instructions = template.replace('{{parkInfo}}', parkInfo);
 
 type ChatResponse = {
    id: string;
@@ -20,6 +29,7 @@ export const chatService = {
    ): Promise<ChatResponse> {
       const response = await client.responses.create({
          model: 'gpt-5-nano',
+         instructions,
          input: prompt,
          reasoning: null,
          previous_response_id:
